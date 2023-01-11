e687987c086ceed1fbd1526b77b4d5a9e93c397c
Balaji28052004 <122265112+Balaji28052004@users.noreply.github.com>
2023-01-11 10:39:46 +0530

    Create Concartenating.py

---
+++ Concartenating.py
@@ -0,0 +1,14 @@
+nested_list = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+length = len(nested_list)
+print("Length of the nested list: ",length)
+list1 = [1, 2, 3]
+list2 = [4, 5, 6]
+concatenated_list = list1 + list2
+print("Concatenated list: ", concatenated_list)
+list3 = [1, 2, 3, 4, 5]
+print(3 in list3)
+for element in list3:
+    print(element)
+print("First element of the list: ", list3[0])
+print("Third element of the list: ", list3[2])
+print("Elements from index 1 to 3: ", list3[1:4])
